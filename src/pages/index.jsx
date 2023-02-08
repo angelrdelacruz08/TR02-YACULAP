4f3e50f57efd7c2003c227dbe91e24b01b06f349
angel <70105696@continental.edu.pe>
2023-02-08 10:07:12 -0500

    4 commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,8 +9,8 @@ export default function Index(){
     <img src="https://consultoraecoambiental.com/wp-content/uploads/2023/02/portada-yaculap.png" alt=""/>
     </div>
     <div class="text-center p-4 flex md:flex-row flex-col gap-3 justify-center">
-      <a href="#" class="inline-block py-3 px-16 bg-black text-white font-bold border-transparent border-8 text-2xl rounded-3xl hover:border-gray-200 hover:bg-white hover:text-black transition duration-200" target="_blank" rel="noreferrer">Descarga YACU-LAP</a>
-      <a href="#" class="inline-block py-3 px-16 bg-red-500 text-white font-bold border-transparent border-8 text-2xl rounded-3xl hover:border-gray-200 hover:bg-white hover:text-black transition duration-200" target="_blank" rel="noreferrer">Nosotros</a>
+      <a href="https://play.google.com/store/apps/details?id=com.paparazziteam.yakulap&hl=es&fbclid=IwAR2oUgQZEsQuWJi9A9_R1hvq-80s9s94Y73HefioQz_Z-r8T0kwXGfr79iw&pli=1" class="inline-block py-3 px-16 bg-black text-white font-bold border-transparent border-8 text-2xl rounded-3xl hover:border-gray-200 hover:bg-white hover:text-black transition duration-200" target="_blank" rel="noreferrer">Descarga YACU-LAP</a>
+      <a href="#" class="inline-block py-3 px-16 bg-red-500 text-white font-bold border-transparent border-8 text-2xl rounded-3xl hover:border-gray-200 hover:bg-white hover:text-black transition duration-200" target="_blank" rel="noreferrer">Conoce mas</a>
     </div>
     <div class="lg:container 2xl:w-3/5 mx-auto md:flex gap-5 md:w-full p-3 ">
                 <div class="left bg-character-mario bg-no-repeat bg-contain bg-top w-5/6 md:w-1/2 pt-52 mx-auto">         
@@ -56,9 +56,9 @@ export default function Index(){
                          </div>*/}
                         <div class="content bg-wave-pattern bg-repeat-x relative -top-3">
                             <h3 class="font-black text-4xl py-8">Explora</h3>
-                            <p class="text-xl">Toma un vistaso</p>
+                            <p class="text-xl">..........</p>
                             <a href="/#" class="xl:text-2xl my-4 inline-block py-2 px-16 bg-red-500 text-white font-black border-transparent border-8 rounded-3xl hover:border-gray-200 hover:bg-white hover:text-black transition duration-200 group">
-                                Take a look! 
+                                Toma un vistazo!
                                 <span class="w-4 h-4 inline-block border-yellow-300 border-solid border-t-[5px] border-r-[5px] transition-all transform rotate-45 group-hover:border-red-500 xl:group-hover:ml-4"></span>
                             </a>
                         </div>
